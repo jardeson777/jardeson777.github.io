49a510e8c149cd33dc4c8d393c030513e4869674
Jardeson <Jardeson777@gmail.com>
2021-01-26 22:51:21 -0300

    Limpei o codigo js, tirei os codigo comentados que estava testando. Corrigi alguns erros na hora de add novo nome na lista

--- Js/script.js
+++ Js/script.js
@@ -9,44 +9,44 @@ function abrindoMenu() {
     conteudo.style = 'display: flex;';
 
     if(lista.length == 0){
-        document.getElementById('list').innerHTML = '<li>Não possui nenhum nome na lista.</li>';
+        document.getElementById('list1').innerHTML = '<li>Não possui nenhum nome na lista.</li>';
     }
 }
 
 function buscar(){
     procurarPessoas.style = 'display: flex;';
     addPessoas.style = 'display: none';
-    //conteudo.style = 'display: none;';
 }
 
 function adicionar(){
     addPessoas.style = 'display: flex;';
     procurarPessoas.style = 'display: none;';
-    //conteudo.style = 'display: none;';
 }
 
 function procurarPessoa(){
-
+    var nomePesquisa = document.getElementById('nome').value;
+    if(lista.indexOf(nomePesquisa)>-1){
+        list1.style = 'display: none';
+        
+    }
 }
 
 function addPessoa(){
     var novaPessoa = document.getElementById('add').value;
-    lista.push('<li>' + contador + 'º ~~> ' + novaPessoa + '</li>');
-    contador++;
+    document.getElementById('add').value = '';
 
-    //procurarPessoas.style = 'display: none;';
-    //addPessoas.style = 'display: none;';
+    if(novaPessoa != '' & novaPessoa != ' '){
+        lista.push('<li>' + contador + 'º ~~> ' + novaPessoa + '</li>');
+    contador++;
+    }
+    
     conteudo.style = 'display: flex;';
 
     if(lista.length > 0){
-        document.getElementById('list').innerHTML = lista;
-        var listaUl = document.getElementById('list');
+        document.getElementById('list1').innerHTML = lista;
+        var listaUl = document.getElementById('list1');
         listaUl.innerHTML = lista.join('')
     }
-}
-
-function mostrarTodos(){
-    
 }
 
 var lista = [];
